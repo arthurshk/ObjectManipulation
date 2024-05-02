d1787b3561450963127052dcd04c38c16af95629
Arthur <arthurdragon39@gmail.com>
2024-05-02 13:37:22 -0500

    Commit

--- Poolhallogo/Logic/PoolGameLogic.cs
+++ Poolhallogo/Logic/PoolGameLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Poolhall.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,7 @@ namespace Poolhall.Logic
         }
         public bool isGameOver()
         {
-
+            return false;
         }
     }
 }
--- Poolhallogo/ViewModel/RelayCommand.cs
+++ Poolhallogo/ViewModel/RelayCommand.cs
@@ -4,5 +4,23 @@ namespace Poolhallogo.ViewModel
 {
     internal class RelayCommand : ICommand
     {
+        public RelayCommand(Action onShoot)
+        {
+            OnShoot = onShoot;
+        }
+
+        public Action OnShoot { get; }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
